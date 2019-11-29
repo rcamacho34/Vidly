907a8a3e683bc97fb2548d534bf8ecef76ae40c9
Ronaldo Camacho <ronaldocamacho@gmail.com>
2019-11-29 10:51:02 -0300

    Segundo commit

---
+++ Vidly/Controllers/CustomersController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class CustomersController : Controller
+    {
+        // GET: Customers
+        public ActionResult Index()
+        {
+            var customers = GetCostumers();
+
+            return View(customers);
+        }
+
+        public ActionResult Details(int id)
+        {
+            Customer customer = GetCostumers().SingleOrDefault(s => s.Id == id);
+
+            if (customer == null)
+                return HttpNotFound();
+
+            return View(customer);
+        }
+
+        private IEnumerable<Customer> GetCostumers()
+        {
+            return new List<Customer>
+            {
+                new Customer { Id = 1, Name = "John Smith" },
+                new Customer { Id = 2, Name = "Mary Williams" }
+            };
+        }
+    }
+}
--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -37,13 +37,28 @@ namespace Vidly.Controllers
         // movies
         public ActionResult Index(int? pageIndex, string sortBy)
         {
-            if (!pageIndex.HasValue)
-                pageIndex = 1;
+            var movies = GetMovies();
 
-            if (String.IsNullOrWhiteSpace(sortBy))
-                sortBy = "Name";
+            return View(movies);
+        }
 
-            return Content(String.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
+        public ActionResult Details(int id)
+        {
+            Movie movie = GetMovies().SingleOrDefault(s => s.Id == id);
+
+            if (movie == null)
+                return HttpNotFound();
+
+            return View(movie);
+        }
+
+        private IEnumerable<Movie> GetMovies()
+        {
+            return new List<Movie>
+            {
+                new Movie { Id = 1, Name = "Shrek!" },
+                new Movie { Id = 2, Name = "Wall-e" }
+            };
         }
 
         [Route("movies/released/{year}/{month:regex(\\d{2}):range(1, 12)}")]
